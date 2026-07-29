25a53dfb892bb21c6e35ec6ab79bdb10271281b0
OlegErshov <olezhka1629@gmail.com>
2026-07-29 11:37:14 +0200

    fix: linter
    Signed-off-by: OlegErshov <olezhka1629@gmail.com>
    
    On-behalf-of: SAP aleh.yarshou@sap.com

--- services/iam-service/cmd/root.go
+++ services/iam-service/cmd/root.go
@@ -20,6 +20,7 @@ import (
 	"github.com/spf13/cobra"
 
 	pmcorev1alpha1 "go.platform-mesh.io/apis/core/v1alpha1"
+	pmprovidersv1alpha1 "go.platform-mesh.io/apis/providers/v1alpha1"
 	platformmeshcontext "go.platform-mesh.io/golang-commons/config"
 	"go.platform-mesh.io/golang-commons/logger"
 	"go.platform-mesh.io/iam-service/pkg/config"
@@ -29,7 +30,6 @@ import (
 
 	kcpapisv1alpha1 "github.com/kcp-dev/sdk/apis/apis/v1alpha1"
 	kcptenancyv1alpha1 "github.com/kcp-dev/sdk/apis/tenancy/v1alpha1"
-	providersv1alpha1 "go.platform-mesh.io/apis/providers/v1alpha1"
 )
 
 var (
@@ -48,7 +48,7 @@ func init() {
 	utilruntime.Must(pmcorev1alpha1.AddToScheme(scheme))
 	utilruntime.Must(kcptenancyv1alpha1.AddToScheme(scheme))
 	utilruntime.Must(kcpapisv1alpha1.AddToScheme(scheme))
-	utilruntime.Must(providersv1alpha1.AddToScheme(scheme))
+	utilruntime.Must(pmprovidersv1alpha1.AddToScheme(scheme))
 	rootCmd.AddCommand(serverCmd)
 
 	defaultCfg = platformmeshcontext.NewDefaultConfig()
