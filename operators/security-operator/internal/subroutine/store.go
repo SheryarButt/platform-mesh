e87a74e44304e926620c477eb4d4420aa8a581f8
Nikita Rudakov <nikita.rudakov@sap.com>
2026-07-31 00:21:12 +0300

    revert security-operator and search-service ListStores pagination
    Scope this PR to golang-commons only. security-operator depends on the
    released golang-commons v0.18.1, which lacks FindStoreIDByName, so the
    isolated Docker build fails to compile. Defer both consumer changes to a
    separate PR after the new golang-commons release is available.
    
    Signed-off-by: Nikita Rudakov <nikita.rudakov@sap.com>

--- operators/security-operator/internal/subroutine/store.go
+++ operators/security-operator/internal/subroutine/store.go
@@ -19,13 +19,13 @@ package subroutine
 import (
 	"context"
 	"fmt"
+	"slices"
 
 	openfgav1 "github.com/openfga/api/proto/openfga/v1"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
 	pmcorev1alpha1 "go.platform-mesh.io/apis/core/v1alpha1"
-	commonsstore "go.platform-mesh.io/golang-commons/fga/store"
 	"go.platform-mesh.io/golang-commons/logger"
 	iclient "go.platform-mesh.io/security-operator/internal/client"
 	"go.platform-mesh.io/subroutines"
@@ -91,13 +91,15 @@ func (s *storeSubroutine) Process(ctx context.Context, obj ctrlruntimeclient.Obj
 	if store.Status.StoreID == "" {
 		log.Info().Msg("Store ID not set, trying to find store by name")
 
-		storeID, found, err := commonsstore.FindStoreIDByName(ctx, s.fga, store.Name)
+		list, err := s.fga.ListStores(ctx, &openfgav1.ListStoresRequest{})
 		if err != nil {
 			return subroutines.OK(), err
 		}
-		if found {
+
+		storeIdx := slices.IndexFunc(list.GetStores(), func(i *openfgav1.Store) bool { return i.GetName() == store.Name })
+		if storeIdx != -1 {
 			log.Info().Msg("Store found, updating store ID")
-			store.Status.StoreID = storeID
+			store.Status.StoreID = list.GetStores()[storeIdx].GetId()
 			return subroutines.OK(), nil
 		}
 
